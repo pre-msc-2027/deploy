3d7c86006e4eccf0b4bb88a24357994d36b29606
germain <germaindev123@gmail.com>
2025-07-15 20:02:37 +0200

    Add init script

--- server_config/run.sh
+++ server_config/run.sh
@@ -1,3 +1,6 @@
+echo "create docker network traefik...";
 docker network create traefik;
+echo "create portainer_data volume";
 docker volume create portainer_data;
+echo "docker compose up -d";
 docker compose up -d
